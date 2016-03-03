f19797ddca7a6e0e440a1e953958b2c07b877fae
Kinsey Ashby <kinseyzaire@gmail.com>
2016-03-02 17:03:20 -0700

    making repo public

--- routes/cars.js
+++ routes/cars.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
 
-// add your routes here
+router.get('/cars', function(req, res, next) {
+  res.render('cars/index', {});
+});
+
+router.get('/cars/new', function(req, res, next) {
+  res.render('cars/new', {});
+});
+
+router.post('/cars', function(req, res, next) {
+  res.redirect('cars/index', {});
+});
+
+router.get('/cars/:id', function(req, res, next) {
+  res.render('cars/show', { carId: req.params.id });
+});
+
+router.get('/cars/:id/edit', function(req, res, next) {
+  res.render('cars/edit', { carId: req.params.id });
+});
+
+router.post('/cars/:id', function(req, res, next) {
+  res.redirect('cars/index', {});
+});
+
+router.delete('/cars/:id/delete', function(req, res, next) {
+  res.redirect('cars/index', {});
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')('localhost/hello-demo');
-var carsCollection = db.get('cars');
+var pg = require('pg');
+
+var app = express();
+
+var connectionString = 'postgres://localhost/mydatabase';
+
+function runQuery (query, callback) {
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) { done() ; console.log(err); return; }
+    client.query(query, function (err, results) {
+      done();
+      if (err) { console.log(err); return; }
+      callback(results);
+    });
+   });
+}
+
+router.get('/', function(req, res){
+  runQuery('SELECT * FROM country', function(results) {
+     res.send(results.rows);
+   });
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express CRUD' });
+// });
 
 module.exports = router;
